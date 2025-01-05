0b2c800962dc424f1623701b5687a497bc7e27a5
Vitor Moscoso <vitormoscoso@gmail.com>
2025-01-05 20:17:52 -0300

    initialize Prisma schema and migration for Post model

---
+++ prisma/migrations/20250105231641_init/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "Post" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "tags" TEXT[],
+    "createdAt" TEXT NOT NULL,
+    "updatedAt" TEXT NOT NULL,
+
+    CONSTRAINT "Post_pkey" PRIMARY KEY ("id")
+);
