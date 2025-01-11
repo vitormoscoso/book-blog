07f3d65731ce89123cc25c083ac6d28ac7dd362e
Vitor Moscoso <vitormoscoso@gmail.com>
2025-01-11 12:24:09 -0300

    feat: add CreatePostInput DTO for post creation

---
+++ src/post/dto/create-post.input.ts
@@ -0,0 +1,13 @@
+import { InputType, Field } from '@nestjs/graphql';
+
+@InputType()
+export class CreatePostInput {
+  @Field()
+  title: string;
+
+  @Field()
+  content: string;
+
+  @Field(() => [String])
+  tags: string[];
+}
