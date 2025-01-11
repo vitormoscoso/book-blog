a12afcb5ce90bbd563e094c846fd72ab10135b28
Vitor Moscoso <vitormoscoso@gmail.com>
2025-01-11 12:24:04 -0300

    feat: add queries and mutations for post management in PostResolver and PostService

--- src/post/post.resolver.ts
+++ src/post/post.resolver.ts
@@ -1,5 +1,6 @@
 import { Inject } from '@nestjs/common';
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { CreatePostInput } from './dto/create-post.input';
 import { Post } from './models/post.model';
 import { PostService } from './post.service';
 
@@ -10,5 +11,22 @@ export class PostResolver {
   @Query(() => [Post])
   async posts(): Promise<Post[]> {
     return this.postService.getAllPosts();
+  }
+
+  @Query(() => Post)
+  async post(@Args('id', { type: () => Number }) id: number): Promise<Post> {
+    return this.postService.getPostById(id);
+  }
+
+  @Mutation(() => Post)
+  async createPost(
+    @Args('createPostInput') createPostInput: CreatePostInput,
+  ): Promise<Post> {
+    return this.postService.createPost(createPostInput);
+  }
+
+  @Mutation(() => Post)
+  async deletePost(@Args('id', { type: () => Number }) id: number): Promise<Post> {
+    return this.postService.deletePost(id);
   }
 }
--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'prisma/prisma.service';
+import { CreatePostInput } from './dto/create-post.input';
+import { Post } from './models/post.model';
 
 @Injectable()
 export class PostService {
@@ -7,5 +9,20 @@ export class PostService {
 
   async getAllPosts() {
     return this.prisma.post.findMany();
+  }
+
+  async getPostById(id: number) {
+    return this.prisma.post.findUnique({ where: { id } });
+  }
+
+  async createPost(createPostInput: CreatePostInput): Promise<Post> {
+    const { title, content, tags } = createPostInput;
+    return this.prisma.post.create({
+      data: { title, content, tags },
+    });
+  }
+
+  async deletePost(id: number): Promise<Post> {
+    return this.prisma.post.delete({ where: { id } });
   }
 }
